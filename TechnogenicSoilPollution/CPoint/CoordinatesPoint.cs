bc7e35e3217c5115ac97b0c838f2603dd17899ae
Gostev Konstantin <47049219+Cyclist-code@users.noreply.github.com>
2021-03-24 18:12:01 +0700

    Adding a connection string and a new class for points

---
+++ TechnogenicSoilPollution/CPoint/CoordinatesPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TechnogenicSoilPollution.CPoint
+{
+    // Класс для координат точек пробоотбора
+    public class CoordinatesPoint
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public CoordinatesPoint() { }
+
+        public CoordinatesPoint(double _x, double _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
