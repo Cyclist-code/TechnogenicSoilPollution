bf683139d100f93b0cddb73d7103e9030286ce22
Gostev Konstantin <47049219+Cyclist-code@users.noreply.github.com>
2021-04-18 17:45:31 +0700

    Creating a class for working with a database

---
+++ TechnogenicSoilPollution/Data/WorkDatabase.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace TechnogenicSoilPollution.Data
+{
+    public static class WorkDatabase
+    {
+        private static SqlConnection sqlConnection = null;
+
+        static WorkDatabase()
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ImpurityEmissionDB"].ConnectionString);
+        }
+
+        #region Загрузка химического элементов в ComboBox
+        public static void LoadElementsCB(ComboBox comboBox)
+        {
+            string selectData = "SELECT Name_element FROM ChemicalElements";
+            System.Data.DataTable dataTable = new System.Data.DataTable();
+            SqlCommand commandSelect = new SqlCommand(selectData, sqlConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(commandSelect);
+            adapter.Fill(dataTable);
+            comboBox.DataSource = dataTable;
+            comboBox.ValueMember = "Name_element";
+            comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+        #endregion
+
+        #region Загрузка годов пробоотбора в ComboBox
+        public static void LoadYearsCB(ComboBox comboBox)
+        {
+            string selectData = "SELECT DISTINCT Year_sampling FROM SamplingPoints";
+            System.Data.DataTable dataTable = new System.Data.DataTable();
+            SqlCommand commandFilling = new SqlCommand(selectData, sqlConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(commandFilling);
+            adapter.Fill(dataTable);
+            comboBox.DataSource = dataTable;
+            comboBox.ValueMember = "Year_sampling";
+            comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+        #endregion
+
+        #region Экспорт данных в Excel
+        public static void ExportDataExcel(DataGridView dataGridView)
+        {
+            Microsoft.Office.Interop.Excel.Application ExcelFile = new Microsoft.Office.Interop.Excel.Application();
+            Workbook workbook = ExcelFile.Workbooks.Add(Type.Missing);
+            Worksheet worksheet = null;
+            worksheet = workbook.ActiveSheet;
+
+            try
+            {
+                for (int i = 1; i < dataGridView.Columns.Count + 1; i++)
+                {
+                    worksheet.Cells[1, i] = dataGridView.Columns[i - 1].HeaderText;
+                    worksheet.Cells[1, i].Font.Bold = true;
+                }
+
+                for (int i = 0; i < dataGridView.Rows.Count - 1; i++)
+                {
+                    for (int j = 0; j < dataGridView.Columns.Count; j++)
+                    {
+                        worksheet.Cells[i + 2, j + 1] = dataGridView.Rows[i].Cells[j].Value;
+                    }
+                }
+
+                worksheet.Columns.EntireColumn.AutoFit();
+                Range ShtRange;
+                ShtRange = worksheet.UsedRange;
+                ShtRange.Borders.ColorIndex = 1;
+                ShtRange.Borders.LineStyle = XlLineStyle.xlContinuous;
+                ShtRange.Borders.Weight = XlBorderWeight.xlThin;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Title = "Сохранение в Excel",
+                Filter = "Документ Excel (*.xlsx)|*xlsx"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    workbook.SaveAs(saveFileDialog.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                        XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                    MessageBox.Show("Документ Excel сохранён в директории: " + Environment.NewLine + saveFileDialog.FileName + ".xlsx",
+                                    "Сохранение документа Excel", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            ExcelFile.Quit();
+        }
+        #endregion
+
+    }
+}
