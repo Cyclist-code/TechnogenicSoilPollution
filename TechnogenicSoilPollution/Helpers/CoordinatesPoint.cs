cbaa0edfac33cb12c0e8b6ca6d2341c9eafbf87c
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-06-18 21:43:47 +0700

    Adding a class for working with point coordinates

---
+++ TechnogenicSoilPollution/Helpers/CoordinatesPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TechnogenicSoilPollution.Helpers
+{
+    public class CoordinatesPoint
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public int numberPoint { get; set; }
+
+        public CoordinatesPoint() { }
+        public CoordinatesPoint(double _x, double _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        public CoordinatesPoint(int number, double _x, double _y)
+        {
+            numberPoint = number;
+            x = _x;
+            y = _y;
+        }
+    }
+}
