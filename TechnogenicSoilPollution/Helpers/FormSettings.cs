054bbfe01e68ec163a29a58b28563e98b74a48ea
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-08-08 10:34:41 +0700

    Adding a method to set the default theme of the application

--- TechnogenicSoilPollution/Helpers/FormSettings.cs
+++ TechnogenicSoilPollution/Helpers/FormSettings.cs
@@ -1,13 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using TechnogenicSoilPollution.UC;
 
 namespace TechnogenicSoilPollution.Helpers
 {
     public static class FormSettings
     {
+        #region Дефолтная тема
+        public static void InstallingAppTheme(MaterialForm materialForm, UCHome home, MaterialCheckBox materialCheckBox)
+        {
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(materialForm);
+            string theme = Properties.Settings.Default.DarkTheme;
 
+            if ((theme == "") || (theme == " ") || (theme == "0"))
+            {
+                materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+                materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+                home.ForeColorLabel = Color.Black;
+            }
+            if (theme == "1")
+            {
+                materialCheckBox.Checked = true;
+                materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+                materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue800, Primary.Blue900, Primary.Blue500, Accent.Blue200, TextShade.WHITE);
+                home.ForeColorLabel = Color.White;
+            }
+        }
+        #endregion
     }
 }
