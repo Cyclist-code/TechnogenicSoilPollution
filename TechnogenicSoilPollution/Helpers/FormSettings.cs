90cdb58d8877af6619a6a0ed030372c058dcdcce
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-08-08 10:38:14 +0700

    Adding a skin selection method

--- TechnogenicSoilPollution/Helpers/FormSettings.cs
+++ TechnogenicSoilPollution/Helpers/FormSettings.cs
@@ -1,8 +1,6 @@
 ﻿using MaterialSkin;
 using MaterialSkin.Controls;
-using System;
 using System.Drawing;
-using System.Windows.Forms;
 using TechnogenicSoilPollution.UC;
 
 namespace TechnogenicSoilPollution.Helpers
@@ -28,6 +26,30 @@ namespace TechnogenicSoilPollution.Helpers
                 materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
                 materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue800, Primary.Blue900, Primary.Blue500, Accent.Blue200, TextShade.WHITE);
                 home.ForeColorLabel = Color.White;
+            }
+        }
+        #endregion
+
+        #region Выбор темы оформления
+        public static void ChoosingTheme(UCHome home, MaterialCheckBox materialCheckBox)
+        {
+            if (materialCheckBox.Checked)
+            {
+                var materialSkinManager = MaterialSkinManager.Instance;
+                materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+                materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue800, Primary.Blue900, Primary.Blue500, Accent.Blue200, TextShade.WHITE);
+                home.ForeColorLabel = Color.White;
+                Properties.Settings.Default.DarkTheme = "1";
+                Properties.Settings.Default.Save();
+            }
+            if (!materialCheckBox.Checked)
+            {
+                var materialSkinManager = MaterialSkinManager.Instance;
+                materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+                materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+                home.ForeColorLabel = Color.Black;
+                Properties.Settings.Default.DarkTheme = "0";
+                Properties.Settings.Default.Save();
             }
         }
         #endregion
