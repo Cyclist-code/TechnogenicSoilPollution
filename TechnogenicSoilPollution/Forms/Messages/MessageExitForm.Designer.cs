56339b5101512a4afc4eca1535a3bff5adc5fb44
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-08-17 10:29:57 +0700

    Setting an icon for the application exit form

--- TechnogenicSoilPollution/Forms/Messages/MessageExitForm.Designer.cs
+++ TechnogenicSoilPollution/Forms/Messages/MessageExitForm.Designer.cs
@@ -31,10 +31,10 @@ namespace TechnogenicSoilPollution.Forms.Messages
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MessageExitForm));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
             this.CancelExitAppButton = new MaterialSkin.Controls.MaterialRaisedButton();
             this.ExitAppButton = new MaterialSkin.Controls.MaterialRaisedButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -48,8 +48,20 @@ namespace TechnogenicSoilPollution.Forms.Messages
             this.panel1.Controls.Add(this.ExitAppButton);
             this.panel1.Location = new System.Drawing.Point(0, 23);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(350, 136);
+            this.panel1.Size = new System.Drawing.Size(350, 137);
             this.panel1.TabIndex = 0;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(77, 35);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(268, 40);
+            this.materialLabel1.TabIndex = 6;
+            this.materialLabel1.Text = "Вы действительно хотите выйти из приложения?";
             // 
             // CancelExitAppButton
             // 
@@ -79,23 +91,13 @@ namespace TechnogenicSoilPollution.Forms.Messages
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(11, 24);
+            this.pictureBox1.Image = global::TechnogenicSoilPollution.Properties.Resources.Question;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 29);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
-            // 
-            // materialLabel1
-            // 
-            this.materialLabel1.Depth = 0;
-            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
-            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel1.Location = new System.Drawing.Point(77, 35);
-            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialLabel1.Name = "materialLabel1";
-            this.materialLabel1.Size = new System.Drawing.Size(268, 40);
-            this.materialLabel1.TabIndex = 6;
-            this.materialLabel1.Text = "Вы действительно хотите выйти из приложения?";
             // 
             // MessageExitForm
             // 
